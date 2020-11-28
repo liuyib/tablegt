579b9bfdaefe48e23b1816d1a4afde48076c184b
liuyib <1656081615@qq.com>
2020-11-28 14:47:49 +0800

    chore: remove suffix '.min' of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].min.js',
+    filename: '[name].js',
     library: 'TableGT',
     libraryTarget: 'commonjs2',
   },
